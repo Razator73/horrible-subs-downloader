d28287d7820ead2793c146b45087bae1fe8b0bbd
Ryan Scott <rscott@bestcompany.com>
2019-10-17 10:36:24 -0600

    Make copy_files importable to other scripts

--- copy_files.py
+++ copy_files.py
@@ -3,13 +3,26 @@ import shutil
 import sys
 from pathlib import Path
 
+from set_logger import define_logger
 
-download_dir = Path(os.environ['DOWNLOAD_DIR'])
-destination_dir = Path(os.environ['DESTINATION_DIR'])
+logger = define_logger(__name__)
 
-check_args = True
-pattern = sys.argv[1] if check_args else 'NO FILES SHOULD MATCH THIS'
 
-copied = [x.name for x in destination_dir.glob(pattern)]
-for file in [x for x in download_dir.glob(pattern) if x.name not in copied]:
-    shutil.copy(file, destination_dir)
+def show_files(pattern):
+    try:
+        download_dir = Path(os.environ['DOWNLOAD_DIR'])
+        destination_dir = Path(os.environ['DESTINATION_DIR'])
+    except KeyError:
+        logger.exception('Failed to pull environment variables')
+        sys.exit(1)
+
+    copied = [x.name for x in destination_dir.glob(pattern)]
+    for file in [x for x in download_dir.glob(pattern) if x.name not in copied]:
+        logger.info(f'Copying {file}...')
+        shutil.copy(file, destination_dir)
+
+
+if __name__ == '__main__':
+    check_args = True
+    show_pattern = sys.argv[1] if check_args else 'NO FILES SHOULD MATCH THIS'
+    show_files(show_pattern)
