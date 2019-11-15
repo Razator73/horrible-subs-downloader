7bbd7b0fbe2725e90330053411424bb63ee7cc05
Ryan Scott <rscott@bestcompany.com>
2019-11-15 14:29:34 -0700

    Send email when torrent finishes with file list

--- torrent_add.py
+++ torrent_add.py
@@ -2,12 +2,13 @@ import logging
 import os
 import sys
 
+import ezgmail
 import transmission_rpc as trpc
 
 from horrible_torrents import wait_for_download
 from set_logger import define_logger
 
-logger = define_logger(__name__, logging.DEBUG, file_name='manual_add.log')
+logger = define_logger(__name__, logging.INFO, file_name='manual_add.log')
 
 magnet_url = sys.argv[1]
 logger.debug(f'Adding {magnet_url}')
@@ -15,7 +16,14 @@ tor_client = trpc.Client(port=os.environ['TRANSMISSION_PORT'],
                          username=os.environ['TRANSMISSION_USERNAME'],
                          password=os.environ['TRANSMISSION_PASSWORD'])
 
-t_id = tor_client.add_torrent(magnet_url).id
-logger.debug(f'Added with id={t_id}')
-wait_for_download([t_id], tor_client)
+torrent = tor_client.add_torrent(magnet_url)
+logger.debug(f'Added with id={torrent.id}')
+wait_for_download([torrent.id], tor_client)
 logger.info(f'Successfully added torrent {magnet_url}')
+ezgmail.init(tokenFile=os.environ.get('GMAIL_TOKEN', 'token.json'),
+             credentialsFile=os.environ.get('GMAIL_CREDENTIALS', 'credentials.json'))
+ezgmail.send(os.environ.get('GMAIL_RECIPIENT', ezgmail.EMAIL_ADDRESS),
+             'Torrent Downloaded',
+             'Hey,\n\n The torrent you set to be downloaded has finished. Files downloaded:\n\n\t' +
+             '\n\t'.join([file['name'] for file in torrent.files().values()]) +
+             '\n\nThanks')
